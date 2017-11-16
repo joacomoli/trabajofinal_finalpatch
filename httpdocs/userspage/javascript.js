d7ab497cdc99ea18f8aa68ee8384bec5fa3c6df0
joacomoli <joaco.molinos.jm@gmail.com>
2017-11-16 18:13:30 -0300

    patch 2.0
    nuevo login arreglado

--- httpdocs/userspage/javascript.js
+++ httpdocs/userspage/javascript.js
@@ -1,3 +1,16 @@
-if (moussePressed === true) {
-  alert("presionaste el boton");
+
+function validar() {
+//nombre = document.getElementsByName('nombre').value;
+//apellido = document.getElementsByName('apellido').value;
+//correo = document.getElementsByName('correo').value;
+password = document.getElementsByName('password').value;
+if (password == "sapereaude") {
+url = 'userspage/index.html';
+}
+if (password == "") {
+
+return false;
+}else {
+  false;
+}
 }
---
+++ httpdocs/userspage/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <script src="javascript.js" charset="utf-8"></script>
+    <link href="css\index.css" rel="stylesheet">
+    <title></title>
+  </head>
+  <body>
+      <h2><p>ingrese aquí la clave</p> </h2>
+      <form class="" action="login.php" method="post" onsubmit="validar()">
+        <input type="password" name="password" value= class="campo_password" id="password">
+        <button type="submit" name="button" class="ingresar" > ingresar </button>
+
+      </form>
+      <?php
+      if ($password == "sapereaude") {
+
+
+
+      }
+
+
+
+       ?>
+<script type="text/javascript">
+function validar() {
+//nombre = document.getElementsByName('nombre').value;
+//apellido = document.getElementsByName('apellido').value;
+//correo = document.getElementsByName('correo').value;
+
+password = document.getElementById('password').value;
+if (password == "sapereaude") {
+url = 'userspage/index.html';
+}
+if (password == "") {
+
+return false;
+}else {
+  false;
+}
+}
+</script>
+  </body>
+</html>
